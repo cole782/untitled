7f28c7adc5ba2adbeaa37702e2bc8923de5516be
cole782 <118213718+cole782@users.noreply.github.com>
2022-12-05 13:49:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    # # # # #
+    . # . # .
+    `)
